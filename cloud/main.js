6e5a15e29662cf657099f09e57443508cd8305ef
Kshitij <kshitij.bantupalli@gmail.com>
2019-03-12 02:35:29 -0400

    Refactor
    Refactored code for user ACL and fixed a bunch of permission stuff.
    Added promises for saves and master key permissions.

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,10 @@
 /*
+Version 1.1
+	Kshitij Bantupalli
+		Refactored code for user ACL and fixed a bunch of permission stuff.
+		Added promises for saves and master key permissions.
+
+
 Version 1.0
 	Last Edit : Kshitij Bantupalli
 	TO DO: Fix potential bugs, need to test it.
@@ -16,20 +22,20 @@ Parse.Cloud.define('hello', function(request, response) {
 	Input : JSON (username, password, email)
 	Output : None.
 */
-Parse.Cloud.define('addUser', function(request, response) {
+// Parse.Cloud.define('addUser', function(request, response) {
 
-	var params = request.params;
-	if (!params.username || !params.email || !params.password)
-		return response.error("Missing parameters: need username, email, password");
+// 	var params = request.params;
+// 	if (!params.username || !params.email || !params.password)
+// 		return response.error("Missing parameters: need username, email, password");
 
-	var user = new Parse.User({
-		username : params.username,
-		password : params.password,
-		email : params.email 
-	});
+// 	var user = new Parse.User({
+// 		username : params.username,
+// 		password : params.password,
+// 		email : params.email 
+// 	});
 
-	user.signUp(null, {useMasterKey : true}).then((prof) => response.success(prof));
-});
+// 	user.signUp(null, {useMasterKey : true}).then((prof) => response.success(prof));
+// });
 
 
 /*
@@ -38,17 +44,14 @@ Parse.Cloud.define('addUser', function(request, response) {
 	Output : None.
 */
 Parse.Cloud.define('addACLUser', function(request, response) {
-	var params = request.params;
-	var user = new Parse.User({
-		username : params.username,
-		password : params.password,
-		email :params.email
-	});
-	var objectId = user._getId();
+	var user = new Parse.User();
+	var objectId = user._getid()
 	var acl = new Parse.ACL(user);
 	var success = user.setACL(acl, set({
-		ACL : objectId
-	}))
+		ACL : objectId,
+		useMasterKey: true
+	}));
+	user.save().then((us) => response.success(us));
 });
 
 
@@ -61,8 +64,10 @@ Parse.Cloud.define('setDeviceACL', function(request, response) {
 	var user = new Parse.user();
 	var device = const new Device();
 	device.set("Name" : params.name)
-	device.setACL(new Parse.ACL(Parse.User.Current()));
-	device.save();
+	device.setACL(new Parse.ACL(Parse.User.Current(), set({
+		useMasterKey: true
+	})));
+	device.save().then((dev) => response.success(dev));
 });
 
 
